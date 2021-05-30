c721cbca26561f0c717490ef21778ce324745d67
zhLeon521 <2321837783@qq.com>
2021-05-30 16:12:19 +0800

    nav top1 over

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-28 11:49:21
- * @LastEditTime: 2021-05-30 15:18:44
+ * @LastEditTime: 2021-05-30 16:10:47
  * @FilePath: \zhleon\pages\index.js
  */
 import SelectorIcon from 'heroicons/outline/selector.svg';
@@ -14,7 +14,16 @@ function VercelLogo({ className }) {
     <svg className={className} viewBox="0 0 1155 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M577.344 0L1154.69 1000H0L577.344 0Z" fill="black" />
     </svg>
+  )
+}
 
+
+function Avatar({ src, alt=' '}) {
+  return (
+    <img
+    className="h-8 w-8 rounded-full border border-gray-200"
+      src={src}
+      alt={alt} />
   )
 }
 export default function Home() {
@@ -23,7 +32,7 @@ export default function Home() {
       {/* 最顶端 */}
       < div className="bg-white">
         <header>
-          <nav className="max-w-5xl mx-auto pt-5">
+          <nav className="max-w-5xl mx-auto flex justify-between items-center pt-4">
             {/* 最上面导航，左侧 */}
             <div className="flex items-center space-x-2">
               <VercelLogo className="h-6" />
@@ -40,37 +49,38 @@ export default function Home() {
                 <Link href="#">
                   <a className="inline-flex items-center space-x-2">
                     <span>
-                      <img
-                        className="h-7 w-7 rounded-full border border-gray-200"
-                        src="/avatar.svg" alt="" />
+                      <Avatar src="/avatar.svg" alt="" />
                     </span>
                     <span className="text-sm text-black-300 font-semibold antialiased">Blueheart  Leon</span>
                   </a>
                 </Link>
-                <button className="inline-flex items-center border rounded-md border-transparent p-0.2 hover:border-gray-200 hover:boder-gray-50 transition ease-in-out duration-200">
+                <button className="inline-flex items-center border rounded border-transparent p-0.6 hover:border-gray-200 hover:boder-gray-50 transition ease-in-out duration-200">
                   <SelectorIcon className="h-5 w-5 text-gray-300 " />
                 </button>
               </span>
             </div>
 
-            <div>
-              <button type='button' >
-                Feedback
-              </button>
-              <Link href="#">
-                <a>Blog</a>
-              </Link>
-              <Link href="#">
-                <a>Support</a>
-              </Link>
-              <Link href="#">
-                <a>Docs</a>
-              </Link>
+            <div className="flex items-center space-x-4">
+              <div className="flex items-center space-x-5">
+                <button type='button'  className="border border-gray-200 rounded px-3 py-1.5 text-sm leading-5 text-gray-600 hover:border-black transition ease-in-out duration-200" >
+                  Feedback
+                </button>
+                <Link href="#">
+                  <a className="inline-block text-sm leading-5 text-gray-600 hover:text-black transition ease-in-out duration-200">Blog</a>
+                </Link>
+                <Link href="#">
+                  <a className="inline-block text-sm leading-5 text-gray-600 hover:text-black transition ease-in-out duration-200">Support</a>
+                </Link>
+                <Link href="#">
+                  <a className="inline-block text-sm leading-5 text-gray-600 hover:text-black transition ease-in-out duration-200">Docs</a>
+                </Link>
+              </div>
+             
               <button type="button">
-                <DotsHorizontal />
+                <DotsHorizontal className="h-5 w-5 text-gray-400"/>
               </button>
               <button type="button">
-                <img src="/avatar.svg" alt="zhLeon" />
+                <Avatar src="/avatar.svg" alt="" /> 
               </button>
             </div>
           </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-05-30 13:50:26
- * @LastEditTime: 2021-05-30 14:50:25
+ * @LastEditTime: 2021-05-30 15:53:57
  * @FilePath: \zhleon\tailwind.config.js
  */
 module.exports = {
@@ -10,7 +10,6 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
     extend: {
       fontFamily: {
         'html': ['font-serif'],
